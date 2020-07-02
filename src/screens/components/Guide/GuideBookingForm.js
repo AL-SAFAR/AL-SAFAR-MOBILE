3a31872acae659aa21c287b25b7ad6d22b26d5c3
Khawaja Muhammad Ali <khawajaali246@gmail.com>
2020-07-02 21:24:11 +0500

    btn upgraded

--- src/screens/components/Guide/GuideBookingForm.js
+++ src/screens/components/Guide/GuideBookingForm.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { ScrollView, TouchableOpacity, View, Text } from "react-native";
+import {
+  ScrollView,
+  TouchableOpacity,
+  View,
+  Text,
+  Dimensions,
+} from "react-native";
+import AwesomeButton from "react-native-really-awesome-button";
 import moment from "moment";
 // import NumericInput from "react-native-numeric-input";
 import { Root, Popup } from "popup-ui"; // import { reduxForm } from "redux-form";
@@ -8,6 +15,7 @@ import { DatePicker } from "native-base";
 import { CreditCardInput } from "react-native-credit-card-input";
 import { connect } from "react-redux";
 import { chargeCustomer } from "../../actions/guideActions";
+const { width, height } = Dimensions.get("window");
 import PropTypes from "prop-types";
 import BookingProcessing from "../layout/BookingProcessing";
 const GuideBookingForm = ({
@@ -20,7 +28,7 @@ const GuideBookingForm = ({
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [paymentDetails, setPaymentDetails] = useState(null);
-  const [processing, setProcessing] = useState(false);
+  // const [processing, setProcessing] = useState(false);
   const maxDate = new Date();
   maxDate.setMonth(maxDate.getMonth() + 3);
   const _onChange = (form) => {
@@ -44,7 +52,7 @@ const GuideBookingForm = ({
     },
     cvc: { maxLength: 3 },
   };
-  const handleSubmit = async () => {
+  const handleSubmit = async (next) => {
     // console.log();
     if (startDate && endDate && paymentDetails) {
       let bookdetails = {
@@ -55,10 +63,11 @@ const GuideBookingForm = ({
         guide: currentGuide,
       };
       // console.log(bookdetails);
-      setProcessing(true);
+      // setProcessing(true);
       chargeCustomer(bookdetails).then((res) => {
-        setProcessing(false);
+        // setProcessing(false);
         if (res === true) {
+          next();
           Popup.show({
             type: "Success",
             title: "Booking Completed",
@@ -100,62 +109,62 @@ const GuideBookingForm = ({
   };
   return (
     <Root>
-      {processing ? (
+      {/* {processing ? (
         <BookingProcessing />
-      ) : (
-        <ScrollView>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "center",
-              alignContent: "center",
-              margin: 20,
-            }}
-          >
-            <View>
-              <View style={globalStyles.input}>
-                <DatePicker
-                  defaultDate={Date.now()}
-                  minimumDate={Date.now()}
-                  maximumDate={maxDate}
-                  locale={"en"}
-                  // onChangeText={handleChange("startDate")}
-                  timeZoneOffsetInMinutes={undefined}
-                  modalTransparent={false}
-                  animationType={"fade"}
-                  androidMode={"default"}
-                  placeHolderText="Stay starts from"
-                  textStyle={{ color: "green" }}
-                  placeHolderTextStyle={{ color: "black" }}
-                  onDateChange={(date) => {
-                    setStartDate(date);
-                    console.log(date);
-                  }}
-                  disabled={false}
-                />
-              </View>
-              <View style={globalStyles.input}>
-                <DatePicker
-                  defaultDate={Date.now()}
-                  minimumDate={Date.now()}
-                  maximumDate={maxDate}
-                  locale={"en"}
-                  // onChangeText={handleChange("startDate")}
-                  timeZoneOffsetInMinutes={undefined}
-                  modalTransparent={false}
-                  animationType={"fade"}
-                  androidMode={"default"}
-                  placeHolderText="Stay ends On"
-                  textStyle={{ color: "green" }}
-                  placeHolderTextStyle={{ color: "black" }}
-                  onDateChange={(date) => {
-                    setEndDate(date);
-                    console.log(date);
-                  }}
-                  disabled={false}
-                />
-              </View>
-              {/* <TextInput
+      ) : ( */}
+      <ScrollView>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignContent: "center",
+            margin: 20,
+          }}
+        >
+          <View>
+            <View style={globalStyles.input}>
+              <DatePicker
+                defaultDate={Date.now()}
+                minimumDate={Date.now()}
+                maximumDate={maxDate}
+                locale={"en"}
+                // onChangeText={handleChange("startDate")}
+                timeZoneOffsetInMinutes={undefined}
+                modalTransparent={false}
+                animationType={"fade"}
+                androidMode={"default"}
+                placeHolderText="Stay starts from"
+                textStyle={{ color: "green" }}
+                placeHolderTextStyle={{ color: "black" }}
+                onDateChange={(date) => {
+                  setStartDate(date);
+                  console.log(date);
+                }}
+                disabled={false}
+              />
+            </View>
+            <View style={globalStyles.input}>
+              <DatePicker
+                defaultDate={Date.now()}
+                minimumDate={Date.now()}
+                maximumDate={maxDate}
+                locale={"en"}
+                // onChangeText={handleChange("startDate")}
+                timeZoneOffsetInMinutes={undefined}
+                modalTransparent={false}
+                animationType={"fade"}
+                androidMode={"default"}
+                placeHolderText="Stay ends On"
+                textStyle={{ color: "green" }}
+                placeHolderTextStyle={{ color: "black" }}
+                onDateChange={(date) => {
+                  setEndDate(date);
+                  console.log(date);
+                }}
+                disabled={false}
+              />
+            </View>
+            {/* <TextInput
                 style={globalStyles.input}
                 placeholder="Review body"
                 onChangeText={props.handleChange("body")}
@@ -167,17 +176,44 @@ const GuideBookingForm = ({
                 onChangeText={props.handleChange("rating")}
                 value={props.values.rating}
               /> */}
-              <CreditCardInput
-                addtionalInputsProps={addtionalInputsProps}
-                onChange={_onChange}
-                validColor="#7CFC00"
-              />
-              <TouchableOpacity onPress={handleSubmit}>
+            <CreditCardInput
+              addtionalInputsProps={addtionalInputsProps}
+              onChange={_onChange}
+              validColor="#7CFC00"
+            />
+            <AwesomeButton
+              style={{
+                marginVertical: 10,
+                alignSelf: "center",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              progress
+              raiseLevel={4}
+              backgroundColor="#FFF"
+              textColor="#0099ff"
+              backgroundDarker="#1073CE"
+              backgroundProgress="#b3e0ff"
+              backgroundPlaceholder="#fff"
+              borderColor="#1073CE"
+              borderWidth={2}
+              borderRadius={100}
+              width={width - 50}
+              textSize={24}
+              progressLoadingTime={10000}
+              onPress={(next) => {
+                handleSubmit(next);
+                /** Do Something **/
+              }}
+            >
+              Confirm Booking
+            </AwesomeButton>
+            {/* <TouchableOpacity onPress={handleSubmit}>
                 <View style={globalStyles.button}>
                   <Text style={globalStyles.buttonText}>Confirm Booking</Text>
                 </View>
-              </TouchableOpacity>
-              {/* <Button
+              </TouchableOpacity> */}
+            {/* <Button
               style={{ marginTop: 10 }}
               title="Confirm Booking"
               color="#0099ff"
@@ -185,10 +221,10 @@ const GuideBookingForm = ({
               // }
               onPress={handleSubmit}
             /> */}
-            </View>
           </View>
-        </ScrollView>
-      )}
+        </View>
+      </ScrollView>
+      {/* )} */}
     </Root>
   );
 };
