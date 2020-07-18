c643d701b1b7a0d37ad50d954f90ea772523425f
Khawaja Muhammad Ali <khawajaali246@gmail.com>
2020-07-18 18:46:09 +0500

    Recommendation screen completed

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -9,13 +9,11 @@ import {
   Dimensions,
   Animated,
 } from "react-native";
+import StarRating from "react-native-star-rating";
 import Slider from "react-native-slider";
+import { Accordion } from "native-base";
 import { globalStyles } from "../../styles/global";
-// import {
-//   Ionicons as Icons,
-//   FontAwesome as FIcons,
-//   Feather as FeatherIcon,
-// } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 // import { TextInput } from "react-native-paper";
 import { ScrollView } from "react-native-gesture-handler";
 // import Slider from "@react-native-community/slider";
@@ -27,13 +25,59 @@ import { Platform } from "@unimodules/core";
 const { height, width } = Dimensions.get("window");
 import AwesomeButton from "react-native-really-awesome-button";
 
+const data = [
+  {
+    hotel: {
+      hotelName: "Pearl Continental",
+      hotelImage:
+        "https://res.cloudinary.com/al-safar435/image/upload/v1594931430/HotelProfile/jbot0i8y7hrphphjlatc.jpg",
+      starRating: 5,
+      location: "Islamabad",
+    },
+    guide: {
+      guideName: "Khawaja Ali",
+      guideImage:
+        "https://res.cloudinary.com/al-safar435/image/upload/v1592678851/TravelGuideImages/wqprs0n2nwvkzv1mdvwn.jpg",
+      starRating: 5,
+      location: "Islamabad",
+    },
+  },
+  {
+    hotel: {
+      hotelName: "Marriot",
+      hotelImage:
+        "https://res.cloudinary.com/al-safar435/image/upload/v1594931431/HotelProfile/rnrzijqmqzqdzxmnr84i.jpg",
+      starRating: 5,
+      location: "Islamabad",
+    },
+    guide: {
+      guideName: "Tahseen Tauseef",
+      guideImage:
+        "https://res.cloudinary.com/al-safar435/image/upload/v1592840583/TravelGuideImages/wruplwn7tsw4e1qwkn0w.jpg",
+      starRating: 5,
+      location: "Islamabad",
+    },
+  },
+];
+
 const Explore = () => {
   const [budget, setBudget] = useState(1000);
   const [totalDays, setTotalDays] = useState(1);
   const [hotelStatus, sethotelStatus] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+  const [showRecom, setshowRecom] = useState(false);
   const [guideStatus, setguideStatus] = useState(false);
+  const [scrollY, setscrollY] = useState(new Animated.Value(0));
+
+  const changeAccordian = () => {
+    if (expanded === true) {
+      setExpanded(false);
+    } else {
+      setExpanded(true);
+    }
+  };
   useEffect(() => {
-    const scrollY = new Animated.Value(0);
+    // const scrollY = new Animated.Value(0);
     let startHeaderHeight = 80;
     let endHeaderHeight = 50;
 
@@ -133,162 +177,287 @@ const Explore = () => {
             </View>
           </View>
           <View style={{ marginTop: 20 }}>
-            <Text
-              style={{
-                fontSize: 24,
-                fontWeight: "700",
-                paddingHorizontal: 20,
+            <TouchableOpacity
+              onPress={() => {
+                changeAccordian();
               }}
-            >
-              Let Us Help You Out!
-            </Text>
-            <View
-              style={{
-                marginTop: 20,
-                marginHorizontal: 20,
-                alignContent: "center",
-              }}
-            >
-              <Text style={{ fontSize: 18, fontWeight: "500" }}>
-                Share your Budget with us.
-              </Text>
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                }}
-              >
-                <Text style={styles.value} numberOfLines={1}>
-                  <Text style={{ fontWeight: "500" }}>Budget: </Text>
-                  Rs.{budget}
-                </Text>
-              </View>
-              <Slider
-                minimumTrackTintColor="#0099ff"
-                minimumValue={1000}
-                maximumValue={100000}
-                step={500}
-                value={budget}
-                onValueChange={(value) => setBudget(value)}
-                thumbImage={require("../../assets/dollar.png")}
-                thumbStyle={styles.thumb}
-                thumbTintColor="#F3CC2F"
-              />
-            </View>
-            <View
-              style={{
-                marginTop: 20,
-                marginHorizontal: 20,
-                alignContent: "center",
-              }}
-            >
-              <Text style={{ fontSize: 18, fontWeight: "500" }}>
-                How long will your trip be ?
-              </Text>
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                }}
-              >
-                <Text style={styles.value} numberOfLines={1}>
-                  <Text style={{ fontWeight: "500" }}>Days: </Text>
-                  {totalDays}
-                </Text>
-              </View>
-              <Slider
-                minimumValue={1}
-                maximumValue={15}
-                step={1}
-                value={totalDays}
-                onValueChange={(value) => setTotalDays(value)}
-                minimumTrackTintColor="#0099ff"
-                maximumTrackTintColor="#d3d3d3"
-                thumbTintColor="#0099ff"
-              />
-            </View>
-            <View
-              style={{
-                marginTop: 20,
-                marginHorizontal: 20,
-                alignContent: "center",
-              }}
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
             >
               <Text
-                style={{ fontSize: 18, fontWeight: "500", marginBottom: 10 }}
+                style={
+                  expanded
+                    ? {
+                        fontSize: 24,
+                        fontWeight: "700",
+                        paddingHorizontal: 20,
+                      }
+                    : {
+                        fontSize: 24,
+                        fontWeight: "700",
+                        paddingHorizontal: 20,
+                        color: "#0099ff",
+                      }
+                }
               >
-                Choose the Services you need.
+                Let Us Help You Out!
               </Text>
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-evenly",
-                  alignItems: "center",
-                }}
-              >
-                <TouchableOpacity
-                  style={hotelStatus ? styles.buttonPress : styles.button}
-                  onPress={() => {
-                    if (hotelStatus === false) {
-                      sethotelStatus(true);
-                    } else {
-                      sethotelStatus(false);
-                    }
+              {expanded ? (
+                <AntDesign name="caretup" size={24} color="black" />
+              ) : (
+                <AntDesign name="caretdown" size={24} color="#0099ff" />
+              )}
+            </TouchableOpacity>
+            {expanded && (
+              <View>
+                <View
+                  style={{
+                    marginTop: 20,
+                    marginHorizontal: 20,
+                    alignContent: "center",
+                  }}
+                >
+                  <Text style={{ fontSize: 18, fontWeight: "500" }}>
+                    Share your Budget with us.
+                  </Text>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Text style={styles.value} numberOfLines={1}>
+                      <Text style={{ fontWeight: "500" }}>Budget: </Text>
+                      Rs.{budget}
+                    </Text>
+                  </View>
+                  <Slider
+                    minimumTrackTintColor="#0099ff"
+                    minimumValue={1000}
+                    maximumValue={100000}
+                    step={500}
+                    value={budget}
+                    onValueChange={(value) => setBudget(value)}
+                    thumbImage={require("../../assets/dollar.png")}
+                    thumbStyle={styles.thumb}
+                    thumbTintColor="#F3CC2F"
+                  />
+                </View>
+                <View
+                  style={{
+                    marginTop: 20,
+                    marginHorizontal: 20,
+                    alignContent: "center",
+                  }}
+                >
+                  <Text style={{ fontSize: 18, fontWeight: "500" }}>
+                    How long will your trip be ?
+                  </Text>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Text style={styles.value} numberOfLines={1}>
+                      <Text style={{ fontWeight: "500" }}>Days: </Text>
+                      {totalDays}
+                    </Text>
+                  </View>
+                  <Slider
+                    minimumValue={1}
+                    maximumValue={15}
+                    step={1}
+                    value={totalDays}
+                    onValueChange={(value) => setTotalDays(value)}
+                    minimumTrackTintColor="#0099ff"
+                    maximumTrackTintColor="#d3d3d3"
+                    thumbTintColor="#0099ff"
+                  />
+                </View>
+                <View
+                  style={{
+                    marginTop: 20,
+                    marginHorizontal: 20,
+                    alignContent: "center",
                   }}
                 >
                   <Text
-                    style={hotelStatus ? styles.welcomePress : styles.welcome}
+                    style={{
+                      fontSize: 18,
+                      fontWeight: "500",
+                      marginBottom: 10,
+                    }}
                   >
-                    Hotel
+                    Choose the Services you need.
                   </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={guideStatus ? styles.buttonPress : styles.button}
-                  onPress={() => {
-                    if (guideStatus === false) {
-                      setguideStatus(true);
-                    } else {
-                      setguideStatus(false);
-                    }
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-evenly",
+                      alignItems: "center",
+                    }}
+                  >
+                    <TouchableOpacity
+                      style={hotelStatus ? styles.buttonPress : styles.button}
+                      onPress={() => {
+                        if (hotelStatus === false) {
+                          sethotelStatus(true);
+                        } else {
+                          sethotelStatus(false);
+                        }
+                      }}
+                    >
+                      <Text
+                        style={
+                          hotelStatus ? styles.welcomePress : styles.welcome
+                        }
+                      >
+                        Hotel
+                      </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={guideStatus ? styles.buttonPress : styles.button}
+                      onPress={() => {
+                        if (guideStatus === false) {
+                          setguideStatus(true);
+                        } else {
+                          setguideStatus(false);
+                        }
+                      }}
+                    >
+                      <Text
+                        style={
+                          guideStatus ? styles.welcomePress : styles.welcome
+                        }
+                      >
+                        Guide
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    marginTop: 20,
+                    marginHorizontal: 20,
+                    justifyContent: "center",
+                    alignItems: "center",
                   }}
                 >
-                  <Text
-                    style={guideStatus ? styles.welcomePress : styles.welcome}
+                  <AwesomeButton
+                    raiseLevel={4}
+                    backgroundColor="#FFF"
+                    textColor="#0099ff"
+                    backgroundDarker="#1073CE"
+                    borderColor="#1073CE"
+                    borderWidth={2}
+                    borderRadius={50}
+                    width={width - 100}
+                    textSize={24}
+                    onPress={(next) => {
+                      // setModalOpen(true);
+                      setshowRecom(true);
+                      /** Do Something **/
+                      next();
+                    }}
                   >
-                    Guide
-                  </Text>
-                </TouchableOpacity>
+                    Get My Package
+                  </AwesomeButton>
+                </View>
               </View>
-            </View>
-            <View
-              style={{
-                marginTop: 20,
-                marginHorizontal: 20,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <AwesomeButton
-                raiseLevel={4}
-                backgroundColor="#FFF"
-                textColor="#0099ff"
-                backgroundDarker="#1073CE"
-                borderColor="#1073CE"
-                borderWidth={2}
-                borderRadius={50}
-                width={width - 100}
-                textSize={24}
-                onPress={(next) => {
-                  // setModalOpen(true);
-                  /** Do Something **/
-                  next();
-                }}
-              >
-                Get My Package
-              </AwesomeButton>
-            </View>
+            )}
+            {showRecom && (
+              <View style={{ flexDirection: "column" }}>
+                {data.map((item, key) => {
+                  key = key + 1;
+                  return (
+                    <View
+                      style={{
+                        borderWidth: 2,
+                        borderRadius: 10,
+                        marginVertical: 20,
+                        borderColor: "rgba(0, 153, 255,0.3)",
+                        marginHorizontal: 20,
+                        padding: 10,
+                      }}
+                      key={key}
+                    >
+                      <Text
+                        style={{
+                          marginTop: 10,
+                          textAlign: "center",
+                          fontSize: 24,
+                          fontWeight: "500",
+                        }}
+                      >
+                        Package {key}
+                      </Text>
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          justifyContent: "space-evenly",
+                          marginTop: 10,
+                        }}
+                      >
+                        <TouchableOpacity style={styles.recomCard}>
+                          <Image
+                            source={{
+                              uri: item.hotel.hotelImage,
+                            }}
+                            resizeMode="contain"
+                            style={{
+                              width: width / 2 - 70,
+                              height: 100,
+                              borderRadius: 10,
+                            }}
+                          />
+                          <Text style={{ fontSize: 18, fontWeight: "500" }}>
+                            {item.hotel.hotelName}
+                          </Text>
+                          <Text style={{ fontSize: 12, fontWeight: "500" }}>
+                            {item.hotel.location}
+                          </Text>
+                          <StarRating
+                            style={{ marginTop: 10 }}
+                            disable
+                            maxStars={5}
+                            rating={item.hotel.starRating}
+                            starSize={15}
+                          />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.recomCard}>
+                          <Image
+                            source={{
+                              uri: item.guide.guideImage,
+                            }}
+                            resizeMode="contain"
+                            style={{
+                              width: width / 2 - 70,
+                              height: 100,
+                              borderRadius: 10,
+                            }}
+                          />
+                          <Text style={{ fontSize: 18, fontWeight: "500" }}>
+                            {item.guide.guideName}
+                          </Text>
+                          <Text style={{ fontSize: 12, fontWeight: "500" }}>
+                            {item.guide.location}
+                          </Text>
+                          <StarRating
+                            style={{ marginTop: 10 }}
+                            disable
+                            maxStars={5}
+                            rating={item.guide.starRating}
+                            starSize={15}
+                          />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  );
+                })}
+              </View>
+            )}
           </View>
         </View>
       </ScrollView>
@@ -340,6 +509,28 @@ var styles = StyleSheet.create({
     backgroundColor: "#0099ff",
     borderWidth: 1,
     borderRadius: 10,
+  },
+  recomCard: {
+    flex: 1,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    paddingVertical: 15,
+    height: 200,
+    backgroundColor: "#f5f5f5",
+    width: width / 2 - 20,
+    borderRadius: 25,
+    borderWidth: null,
+    // justifyContent: "center",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+    elevation: 6,
   },
 });
 export default Explore;
